06392e7e91f53d0ec4c1d93811ff7410294abc7a
Jenna <134770484+jennasmithx@users.noreply.github.com>
2026-08-15 15:31:21 +0200

    Update cart.js

--- public/cart.js
+++ public/cart.js
@@ -98,7 +98,14 @@ function renderCart() {
     </div>
     <div class="cart-line-right">
       <div class="cart-line-price">R ${(i.price * i.qty).toFixed(2)}</div>
-      <button class="cart-remove-btn" onclick="removeFromCart('${i.id}')" aria-label="Remove ${i.name}">🗑</button>
+      <button class="cart-remove-btn" onclick="removeFromCart('${i.id}')" aria-label="Remove ${i.name}">
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+          <path d="M3 6h18" />
+          <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+          <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+          <path d="M10 11v6M14 11v6" />
+        </svg>
+      </button>
     </div>
   </div>
 `).join('');
