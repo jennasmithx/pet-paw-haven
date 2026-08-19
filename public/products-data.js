08db7031cf163631a54f067908534fec1f40adb7
Jenna <134770484+jennasmithx@users.noreply.github.com>
2026-08-19 12:50:09 +0200

    Update products-data.js

--- public/products-data.js
+++ public/products-data.js
@@ -25,7 +25,7 @@ const PRODUCTS = {
   },
 "3": {
   name: "Pet Water Fountain",
-  price: 360.00,
+  price: 390.00,
   tag: "Hydration",
   description: "A convenient flowing water fountain designed to keep your pet's water fresh and encourage healthy hydration throughout the day.",
 image: "images/petfountain01.jpeg",
